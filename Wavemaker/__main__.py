f1eb9007835a1735daf2f88adf75ddfb85a93579
Rosabel <imalonelynerd@gmail.com>
2025-04-23 11:21:45 +0200

    Replaced homemade logger with loguru

--- Wavemaker/__main__.py
+++ Wavemaker/__main__.py
@@ -1,4 +1,5 @@
-from Wavemaker.tools.Console import Console
+from loguru import logger
+
 from Wavemaker.tools.Mixer import Mixer
 from Wavemaker.tools.Node import Node
 
@@ -15,101 +16,107 @@ destFolder = root.go("dist").makeDir()
 
 
 def defineActive():
-    activeLocation = auroraFolder.go("active.txt")
-    activeLocation.write(defaultVariant)
-    Console.success(f"Default variant ({defaultVariant}) defined.")
+	activeLocation = auroraFolder.go("active.txt")
+	activeLocation.write(defaultVariant)
+	logger.success(f"Default variant ({defaultVariant}) defined.")
 
 
 def zipAssets():
-    zipLocation = auroraFolder.go("assets.muxzip")
-    zipLocation.delete()
-    consoleIcons = resourcesFolder.go("consoleIcons")
-    consoleIcons.zip(zipLocation.path(), "muxzip")
-    Console.success("Assets zipped.")
+	zipLocation = auroraFolder.go("assets.muxzip")
+	zipLocation.delete()
+	consoleIcons = resourcesFolder.go("consoleIcons")
+	consoleIcons.zip(zipLocation.path(), "muxzip")
+	logger.success("Assets zipped.")
 
 
 def createSchemesForVariants():
-    colorsDataFolder = resourcesFolder.go(["data", "colors"])
-    globalSchemeTemplate = resourcesFolder.go(["schemes", "global.ini"])
-    for variant in variants:
-        colorsDataFile = colorsDataFolder.go(f"{variant}.toml")
-        globalSchemeFile = variantsFolder.go([variant, "theme", "active", "scheme", "global.ini"])
-        result = Mixer(
-            template=globalSchemeTemplate.read(),
-            data=colorsDataFile.read(),
-        ).mix()
-        globalSchemeFile.write(result)
-        Console.success(f"Schemes created for {variant}.")
+	colorsDataFolder = resourcesFolder.go(["data", "colors"])
+	globalSchemeTemplate = resourcesFolder.go(["schemes", "global.ini"])
+	for variant in variants:
+		colorsDataFile = colorsDataFolder.go(f"{variant}.toml")
+		globalSchemeFile = variantsFolder.go(
+			[variant, "theme", "active", "scheme", "global.ini"]
+		)
+		result = Mixer(
+			template=globalSchemeTemplate.read(),
+			data=colorsDataFile.read(),
+		).mix()
+		globalSchemeFile.write(result)
+		logger.success(f"Schemes created for {variant}.")
 
 
 def copyActive():
-    defaultVariantFolder = variantsFolder.go([defaultVariant, "theme", "active"])
-    defaultVariantFolder.mergeInto(auroraFolder.path(), overwrite=True)
-    Console.success(f"Default variant ({defaultVariant}) copied.")
+	defaultVariantFolder = variantsFolder.go(
+		[defaultVariant, "theme", "active"]
+	)
+	defaultVariantFolder.mergeInto(auroraFolder.path(), overwrite=True)
+	logger.success(f"Default variant ({defaultVariant}) copied.")
 
 
 def setOverlays():
-    overlaysFolder = resourcesFolder.go("overlays")
-    for resolution in supportedResolutions:
-        imagesFolder = auroraFolder.go([resolution, "image"])
-        overlayImage = overlaysFolder.go(f"{resolution}.png")
-        _, newImage = overlayImage.copy(imagesFolder.path())
-        newImage.rename("overlay.png", True)
-    Console.success("Overlays set.")
+	overlaysFolder = resourcesFolder.go("overlays")
+	for resolution in supportedResolutions:
+		imagesFolder = auroraFolder.go([resolution, "image"])
+		overlayImage = overlaysFolder.go(f"{resolution}.png")
+		_, newImage = overlayImage.copy(imagesFolder.path())
+		newImage.rename("overlay.png", True)
+	logger.success("Overlays set.")
 
 
 def createSchemesForResolutions():
-    resolutionsDataFolder = resourcesFolder.go(["data", "resolutions"])
-    defaultSchemeTemplate = resourcesFolder.go(["schemes", "default.ini"])
-    for resolution in supportedResolutions:
-        resolutionsDataFile = resolutionsDataFolder.go(f"{resolution}.toml")
-        defaultSchemeFile = auroraFolder.go([resolution, "scheme"]).makeDir().go("default.ini")
-        result = Mixer(
-            template=defaultSchemeTemplate.read(),
-            data=resolutionsDataFile.read()
-        ).mix()
-        defaultSchemeFile.write(result)
-        Console.success(f"Schemes created for {resolution}.")
+	resolutionsDataFolder = resourcesFolder.go(["data", "resolutions"])
+	defaultSchemeTemplate = resourcesFolder.go(["schemes", "default.ini"])
+	for resolution in supportedResolutions:
+		resolutionsDataFile = resolutionsDataFolder.go(f"{resolution}.toml")
+		defaultSchemeFile = (
+			auroraFolder.go([resolution, "scheme"]).makeDir().go("default.ini")
+		)
+		result = Mixer(
+			template=defaultSchemeTemplate.read(),
+			data=resolutionsDataFile.read(),
+		).mix()
+		defaultSchemeFile.write(result)
+		logger.success(f"Schemes created for {resolution}.")
 
 
 def copyMuxSchemes():
-    schemesFolder = resourcesFolder.go("schemes")
-    muxSchemes = schemesFolder.path().glob("mux*.ini")
-    activeSchemesFolder = auroraFolder.go("scheme")
-    for scheme in muxSchemes:
-        Node(scheme).copy(activeSchemesFolder.path())
-    Console.success(f"Schemes copied in {activeSchemesFolder.path().name}.")
+	schemesFolder = resourcesFolder.go("schemes")
+	muxSchemes = schemesFolder.path().glob("mux*.ini")
+	activeSchemesFolder = auroraFolder.go("scheme")
+	for scheme in muxSchemes:
+		Node(scheme).copy(activeSchemesFolder.path())
+	logger.success(f"Schemes copied in {activeSchemesFolder.path().name}.")
 
 
 def zipVariants():
-    auroraVariantFolder = auroraFolder.go("alternate")
-    for variant in variants:
-        srcVariant = variantsFolder.go(variant)
-        dstZip = auroraVariantFolder.go(f"{variant}.muxzip")
-        srcVariant.zip(dstZip.path(), "muxzip")
-        Console.success(f"{variant} variant zipped.")
+	auroraVariantFolder = auroraFolder.go("alternate")
+	for variant in variants:
+		srcVariant = variantsFolder.go(variant)
+		dstZip = auroraVariantFolder.go(f"{variant}.muxzip")
+		srcVariant.zip(dstZip.path(), "muxzip")
+		logger.success(f"{variant} variant zipped.")
 
 
 def copyGif():
-    gifFile = resourcesFolder.go("gifs").go("preview.gif")
-    auroraDefaultPreviewFolder = auroraFolder.go("640x480").makeDir()
-    gifFile.copy(auroraDefaultPreviewFolder.path())
-    Console.success("Gif copied.")
+	gifFile = resourcesFolder.go("gifs").go("preview.gif")
+	auroraDefaultPreviewFolder = auroraFolder.go("640x480").makeDir()
+	gifFile.copy(auroraDefaultPreviewFolder.path())
+	logger.success("Gif copied.")
 
 
 def zipAurora():
-    auroraFolder.zip(destFolder.go("Aurora.muxthm").path(), "muxthm")
-    Console.success("Aurora zipped.")
+	auroraFolder.zip(destFolder.go("Aurora.muxthm").path(), "muxthm")
+	logger.success("Aurora zipped.")
 
 
 if __name__ == "__main__":
-    defineActive()
-    zipAssets()
-    createSchemesForVariants()
-    copyActive()
-    setOverlays()
-    createSchemesForResolutions()
-    copyMuxSchemes()
-    zipVariants()
-    copyGif()
-    zipAurora()
+	defineActive()
+	zipAssets()
+	createSchemesForVariants()
+	copyActive()
+	setOverlays()
+	createSchemesForResolutions()
+	copyMuxSchemes()
+	zipVariants()
+	copyGif()
+	zipAurora()
--- Wavemaker/tools/Node.py
+++ Wavemaker/tools/Node.py
@@ -4,217 +4,221 @@ from pathlib import Path
 from shutil import rmtree
 from typing import Self
 
-from Wavemaker.tools.Console import Console
+from loguru import logger
 
 NodePath = Path | str
 
 
 class Node:
-    __path: Path
+	__path: Path
 
-    def __init__(self, path: NodePath):
-        self.__path = self.__toPath(path)
+	def __init__(self, path: NodePath):
+		self.__path = self.__toPath(path)
 
-    def exists(self, asFile=False, asFolder=False) -> bool:
-        match (asFile, asFolder):
-            case (True, True):
-                return False
-            case (False, True):
-                return self.__path.is_dir()
-            case (True, False):
-                return self.__path.is_file()
-            case _:
-                return self.__path.exists()
+	def exists(self, asFile=False, asFolder=False) -> bool:
+		match (asFile, asFolder):
+			case (True, True):
+				return False
+			case (False, True):
+				return self.__path.is_dir()
+			case (True, False):
+				return self.__path.is_file()
+			case _:
+				return self.__path.exists()
 
-    def delete(self) -> Self:
-        if not self.__path.exists():
-            Console.warning(f"{self.__path} does not exist, it can't be deleted")
-            return self
-        if self.__path.is_dir():
-            rmtree(self.__path)
-        else:
-            unlink(self.__path)
-        return self
+	def delete(self) -> Self:
+		if not self.__path.exists():
+			logger.warning(f"{self.__path} does not exist, it can't be deleted")
+			return self
+		if self.__path.is_dir():
+			rmtree(self.__path)
+		else:
+			unlink(self.__path)
+		return self
 
-    def clear(self) -> Self:
-        if not self.__path.exists():
-            Console.warning(f"{self.__path} does not exist, it can't be cleared")
-        else:
-            for file in self.__path.glob("*"):
-                if file.is_file():
-                    unlink(file)
-                else:
-                    rmtree(file)
-        return self
+	def clear(self) -> Self:
+		if not self.__path.exists():
+			logger.warning(f"{self.__path} does not exist, it can't be cleared")
+		else:
+			for file in self.__path.glob("*"):
+				if file.is_file():
+					unlink(file)
+				else:
+					rmtree(file)
+		return self
 
-    def read(self) -> str:
-        if not self.__path.exists():
-            Console.warning(f"{self.__path} does not exist, it can't be read")
-            return ""
-        if self.__path.is_dir():
-            Console.warning(f"{self.__path} is a folder")
-            return ""
-        with self.__path.open(mode="r", encoding="utf-8") as f:
-            return f.read()
+	def read(self) -> str:
+		if not self.__path.exists():
+			logger.warning(f"{self.__path} does not exist, it can't be read")
+			return ""
+		if self.__path.is_dir():
+			logger.warning(f"{self.__path} is a folder")
+			return ""
+		with self.__path.open(mode="r", encoding="utf-8") as f:
+			return f.read()
 
-    def write(self, content: str) -> Self:
-        if self.__path.is_dir():
-            Console.warning(f"{self.__path} is a folder")
-            return self
-        try:
-            with self.__path.open(mode="w", encoding="utf-8") as f:
-                f.write(content)
-        except IOError as e:
-            Console.error(f"Unable to edit '{self.__path}': {e}")
-        return self
+	def write(self, content: str) -> Self:
+		if self.__path.is_dir():
+			logger.warning(f"{self.__path} is a folder")
+			return self
+		try:
+			with self.__path.open(mode="w", encoding="utf-8") as f:
+				f.write(content)
+		except IOError as e:
+			logger.error(f"Unable to edit '{self.__path}': {e}")
+		return self
 
-    def makeDir(self) -> Self:
-        if self.__path.exists():
-            Console.warning(f"{self.__path} already exists")
-        else:
-            makedirs(self.__path)
-        return self
+	def makeDir(self) -> Self:
+		if self.__path.exists():
+			logger.warning(f"{self.__path} already exists")
+		else:
+			makedirs(self.__path)
+		return self
 
-    def mergeInto(self, destDir: Path, overwrite=False) -> Self:
-        if not self.__path.exists():
-            Console.warning(f"{self.__path} does not exist")
-            return self
-        if self.__path.is_file():
-            Console.warning(f"{self.__path} is a file")
-            return self
+	def mergeInto(self, destDir: Path, overwrite=False) -> Self:
+		if not self.__path.exists():
+			logger.warning(f"{self.__path} does not exist")
+			return self
+		if self.__path.is_file():
+			logger.warning(f"{self.__path} is a file")
+			return self
 
-        if not destDir.exists():
-            makedirs(destDir)
+		if not destDir.exists():
+			makedirs(destDir)
 
-        if destDir.is_file():
-            Console.warning(f"{destDir} is a file")
-            return self
+		if destDir.is_file():
+			logger.warning(f"{destDir} is a file")
+			return self
 
-        for root, _, files in self.__path.walk():
-            for file in files:
-                srcFile: Path = root / file
-                dstFile: Path = destDir / srcFile.relative_to(self.__path)
+		for root, _, files in self.__path.walk():
+			for file in files:
+				srcFile: Path = root / file
+				dstFile: Path = destDir / srcFile.relative_to(self.__path)
 
-                if not dstFile.parent.exists():
-                    dstFile.parent.mkdir(parents=True, exist_ok=True)
-                replaceable = dstFile.exists() and overwrite
-                if not dstFile.exists() or replaceable:
-                    if replaceable:
-                        Console.warning(f"File {dstFile} already exists. Overwriting...")
-                    shutil.copy2(srcFile, dstFile)
-                else:
-                    Console.warning(f"File {dstFile.name} already exists")
-        return self
+				if not dstFile.parent.exists():
+					dstFile.parent.mkdir(parents=True, exist_ok=True)
+				replaceable = dstFile.exists() and overwrite
+				if not dstFile.exists() or replaceable:
+					if replaceable:
+						logger.info(
+							f"File {dstFile} already exists. Overwriting..."
+						)
+					shutil.copy2(srcFile, dstFile)
+				else:
+					logger.warning(f"File {dstFile.name} already exists")
+		return self
 
-    def change(self, path: Path):
-        self.__path = path
-        return self
+	def change(self, path: Path):
+		self.__path = path
+		return self
 
-    def go(self, path: str | list[str]) -> Self:
-        if type(path) == list:
-            return Node(self.__path / "/".join(path))
-        else:
-            return Node(self.__path / path)
+	def go(self, path: str | list[str]) -> Self:
+		if type(path) == list:
+			return Node(self.__path / "/".join(path))
+		else:
+			return Node(self.__path / path)
 
-    def back(self) -> Self:
-        return Node(Path(self.__path.parent))
+	def back(self) -> Self:
+		return Node(Path(self.__path.parent))
 
-    def container(self) -> Self:
-        if not self.__path.exists():
-            Console.warning(f"{self.__path} does not exist")
-            return self
-        elif self.__path.is_file():
-            return Node(Path(self.__path.parent))
-        else:
-            return self
+	def container(self) -> Self:
+		if not self.__path.exists():
+			logger.warning(f"{self.__path} does not exist")
+			return self
+		elif self.__path.is_file():
+			return Node(Path(self.__path.parent))
+		else:
+			return self
 
-    def path(self) -> Path:
-        return self.__path
+	def path(self) -> Path:
+		return self.__path
 
-    def zip(self, to: Path | None = None, fileFormat: str = "zip") -> Self:
-        if not self.__path.exists():
-            Console.warning(f"{self.__path} does not exist")
-            return self
-        if self.__path.is_file():
-            Console.warning(f"{self.__path} is a file")
-            return self
+	def zip(self, to: Path | None = None, fileFormat: str = "zip") -> Self:
+		if not self.__path.exists():
+			logger.warning(f"{self.__path} does not exist")
+			return self
+		if self.__path.is_file():
+			logger.warning(f"{self.__path} is a file")
+			return self
 
-        if to is None:
-            to = self.__path.parent
-        if to.is_dir():
-            Console.warning(f"{to} is a folder")
-            return self
-        if not (p := to.parent).exists():
-            makedirs(p)
+		if to is None:
+			to = self.__path.parent
+		if to.is_dir():
+			logger.warning(f"{to} is a folder")
+			return self
+		if not (p := to.parent).exists():
+			makedirs(p)
 
-        fullDestPath = str(to.parent / to.stem)
-        shutil.make_archive(fullDestPath, "zip", root_dir=self.__path, base_dir=".")
-        if fileFormat != "zip":
-            shutil.move(f"{fullDestPath}.zip", f"{fullDestPath}.{fileFormat}")
-        return self
+		fullDestPath = str(to.parent / to.stem)
+		shutil.make_archive(
+			fullDestPath, "zip", root_dir=self.__path, base_dir="."
+		)
+		if fileFormat != "zip":
+			shutil.move(f"{fullDestPath}.zip", f"{fullDestPath}.{fileFormat}")
+		return self
 
-    def move(self, to: NodePath) -> Self:
-        to = self.__toPath(to)
+	def move(self, to: NodePath) -> Self:
+		to = self.__toPath(to)
 
-        if to.is_file():
-            Console.warning(f"{to} is a file")
-            return self
-        if not to.exists():
-            makedirs(to)
+		if to.is_file():
+			logger.warning(f"{to} is a file")
+			return self
+		if not to.exists():
+			makedirs(to)
 
-        dest = to / self.__path.name
+		dest = to / self.__path.name
 
-        if dest.exists():
-            Console.warning(f"{dest} already exists. Overwriting...")
+		if dest.exists():
+			logger.info(f"{dest} already exists. Overwriting...")
 
-        shutil.move(self.__path, to)
-        self.__path = to
-        return self
+		shutil.move(self.__path, to)
+		self.__path = to
+		return self
 
-    def copy(self, to: NodePath) -> (Self, Self):
-        to = self.__toPath(to)
+	def copy(self, to: NodePath) -> (Self, Self):
+		to = self.__toPath(to)
 
-        if to.is_file():
-            Console.error(f"{to} is a file")
-            return self, None
-        if not to.exists():
-            makedirs(to)
+		if to.is_file():
+			logger.error(f"{to} is a file")
+			return self, None
+		if not to.exists():
+			makedirs(to)
 
-        dest = to / self.__path.name
+		dest = to / self.__path.name
 
-        if dest.exists():
-            Console.warning(f"{dest} already exists. Overwriting...")
+		if dest.exists():
+			logger.info(f"{dest} already exists. Overwriting...")
 
-        shutil.copy(self.__path, to)
-        copiedElement = Node(dest)
-        return self, copiedElement
+		shutil.copy(self.__path, to)
+		copiedElement = Node(dest)
+		return self, copiedElement
 
-    def rename(self, newName: str, force=False) -> Self:
-        if not self.__path.exists():
-            Console.error(f"{self.__path} does not exist")
-            return self
+	def rename(self, newName: str, force=False) -> Self:
+		if not self.__path.exists():
+			logger.error(f"{self.__path} does not exist")
+			return self
 
-        src = self.__path
-        dst = self.__path.parent / newName
+		src = self.__path
+		dst = self.__path.parent / newName
 
-        if dst.exists():
-            if force:
-                Console.warning(f"{dst} already exists. Overwriting...")
-                Node(dst).delete()
-            else:
-                Console.error(f"{dst} already exists. Backing up...")
-                dst.rename(self.__path.parent / f"{dst.name}.bak")
-        self.__path = src.rename(dst)
-        return self
+		if dst.exists():
+			if force:
+				logger.info(f"{dst} already exists. Overwriting...")
+				Node(dst).delete()
+			else:
+				logger.error(f"{dst} already exists. Backing up...")
+				dst.rename(self.__path.parent / f"{dst.name}.bak")
+		self.__path = src.rename(dst)
+		return self
 
-    def __str__(self):
-        return str(self.__path)
+	def __str__(self):
+		return str(self.__path)
 
-    @staticmethod
-    def __toPath(elem: NodePath) -> Path:
-        if isinstance(elem, Path):
-            return elem
-        elif type(elem) == str:
-            return Path(elem)
-        else:
-            raise TypeError("Path must be of type Path or str")
+	@staticmethod
+	def __toPath(elem: NodePath) -> Path:
+		if isinstance(elem, Path):
+			return elem
+		elif type(elem) == str:
+			return Path(elem)
+		else:
+			raise TypeError("Path must be of type Path or str")
